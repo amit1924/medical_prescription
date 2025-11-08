eeca953081ecf9c0a1b24734972c1836aad662ef
Amit Kumar <amit192400@gmail.com>
2025-11-08 15:38:06 +0530

    added backend and frontend url

--- server/api/index.js
+++ server/api/index.js
@@ -16,7 +16,11 @@ app.use(bodyParser.json());
 // CORS configuration
 app.use(
   cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+    origin: [
+      'http://localhost:5500',
+      'http://127.0.0.1:5500',
+      'https://medical-prescription-frontend.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
