1f4a5c5e4a5f0749f6abe3be89fec4e7a832e280
Amit Kumar <amit192400@gmail.com>
2025-11-08 13:27:08 +0530

    using vercel url in cors

--- api/index.js
+++ api/index.js
@@ -302,8 +302,6 @@ app.use((err, req, res, next) => {
 });
 
 // ----------------- START SERVER -----------------
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
