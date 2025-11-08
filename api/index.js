caa1dbd5759891ed1f65a981e2241c9762565bef
Amit Kumar <amit192400@gmail.com>
2025-11-08 14:31:29 +0530

    fixed cors issues

--- api/index.js
+++ api/index.js
@@ -53,7 +53,11 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(
   cors({
-    origin: true, // Allow all origins in development
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://medical-prescription-neon.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
